908a2a9993785a7d6eecd9aed75280236a7d1365
shSachdeva <ssachdeva@griddynamics.com>
2018-08-27 17:47:58 -0700

    Refactor Code

--- src/main/scala/com/gd/twitterstreamingtToDatalake/TransformTweets.scala
+++ src/main/scala/com/gd/twitterstreamingtToDatalake/TransformTweets.scala
@@ -3,12 +3,16 @@ package com.gd.twitterstreamingtToDatalake
 import org.apache.spark.streaming.dstream.DStream
 import twitter4j.Status
 
-object transformTweets {
+object TransformTweets {
 
   def getText(englishTweets: DStream[Status]): DStream[String] = {
-
     val hashTags = englishTweets.flatMap(x => x.getText.split(" ").filter(_.startsWith("#")))
 
     hashTags
+  }
+
+  // This is the sample operation we are testing
+  def tokenize(f: DStream[String]): DStream[String] = {
+    f.flatMap(_.split(" "))
   }
 }
--- src/main/scala/com/gd/twitterstreamingtToDatalake/TweetsIngestion.scala
+++ src/main/scala/com/gd/twitterstreamingtToDatalake/TweetsIngestion.scala
@@ -5,20 +5,21 @@ import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.twitter.TwitterUtils
 import twitter4j.Status
 
-
 object TweetsIngestion {
 
-  def getTweets(arr:Array[String]): DStream[Status] = {
+  def configureTwitter(arr: Array[String]) = {
 
     System.setProperty("twitter4j.oauth.consumerKey", arr(0))
     System.setProperty("twitter4j.oauth.consumerSecret", arr(1))
     System.setProperty("twitter4j.oauth.accessToken", arr(2))
     System.setProperty("twitter4j.oauth.accessTokenSecret", arr(3))
 
-    //val filters: Seq[String] = Seq("narendramodi")
+  }
 
-    val tweets = TwitterUtils.createStream(ssc, None)//, filters)
+  def getTweets = {
 
+    val filters: Seq[String] = Seq("uscis")
+    val tweets = TwitterUtils.createStream(ssc, None, filters)
     val englishTweets = tweets.filter(_.getLang() == "en")
 
     englishTweets
