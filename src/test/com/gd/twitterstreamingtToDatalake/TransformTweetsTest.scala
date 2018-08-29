7f99f690027e953d52283c0a24ff4719e1311887
shSachdeva <ssachdeva@griddynamics.com>
2018-08-29 15:05:31 -0700

    Add Test for getText function of Tweets Transformation
    * Resolved issue of Twitter4j.Status Mocking

--- src/test/com/gd/twitterstreamingtToDatalake/TransformTweetsTest.scala
+++ src/test/com/gd/twitterstreamingtToDatalake/TransformTweetsTest.scala
@@ -1,30 +1,31 @@
 package com.gd.twitterstreamingtToDatalake
 
 import com.holdenkarau.spark.testing.StreamingSuiteBase
-import org.scalatest.{FunSuite, WordSpec}
-
+import org.scalatest.{FunSuite}
 import twitter4j.Status
 import org.scalamock.scalatest.MockFactory
 
-class TransformTweetsTest extends FunSuite with StreamingSuiteBase with MockFactory{
+class TransformTweetsTest extends FunSuite with StreamingSuiteBase with MockFactory {
 
-  val mockedStatus:Status = mock[twitter4j.Status]
+  test("Should Retreive Only Text from the Tweets Status") {
+    val mockedStatus: Status = mock[twitter4j.Status]
+    (mockedStatus.getText _).expects().returning("This is the #Sample #text for #test purpose")
 
-  var input = new List[mockedStatus]
+    val input = List(List(mockedStatus))
+    val expected = List(List("This is the #Sample #text for #test purpose"))
+    testOperation(input, TransformTweets.getText _, expected, ordered = false)
+  }
 
-    test("Filter only words Starting with #")  {
-      val input = List(List("#firstHash","11","22"), List("this","is","#secondHash"), List("#thirdHash","44","55"))
-      val expected = List(List("#firstHash"),List("#secondHash"),List("#thirdHash"))
+  test("Should Filter the HashTags from the Text Messages") {
+    val input = List(List("#firstHash", "11", "22"), List("this", "is", "#secondHash"), List("#thirdHash", "44", "55"))
+    val expected = List(List("#firstHash"), List("#secondHash"), List("#thirdHash"))
 
-      testOperation(input, TransformTweets.getHashTags _, expected, ordered = false)
-    }
+    testOperation(input, TransformTweets.getHashTags _, expected, ordered = false)
+  }
 
-  test("really simple transformation") {
+  test("Sample Transformation to check the StreamingTestSuite") {
     val input = List(List("hi"), List("hi there you"), List("bye"))
     val expected = List(List("hi"), List("hi", "there", "you"), List("bye"))
     testOperation(input, TransformTweets.tokenize _, expected, ordered = false)
   }
-
-
-
 }
