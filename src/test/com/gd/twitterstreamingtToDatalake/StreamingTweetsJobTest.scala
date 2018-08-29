1033db923894c1b18cfbd333cef4c1818924b4d0
shSachdeva <ssachdeva@griddynamics.com>
2018-08-29 15:06:48 -0700

    Updated Appropriate Names of the Test Cases

--- src/test/com/gd/twitterstreamingtToDatalake/StreamingTweetsJobTest.scala
+++ src/test/com/gd/twitterstreamingtToDatalake/StreamingTweetsJobTest.scala
@@ -2,27 +2,25 @@ package com.gd.twitterstreamingtToDatalake
 
 import org.scalatest.FunSuite
 
-
 class StreamingTweetsJobTest extends FunSuite {
 
-  test("Correct Number of Arguments for Main") {
+  test("Should Validate Configuration if Number of Arguments is four or more") {
     val input = Array("aa", "bb", "bb", "cc")
     assert(StreamingTweetsJob.validateConfiguraiton(input) === false)
   }
 
-  test("Incorrect Number of Arguments for Main1") {
+  test("Should Fail the Validation with Error Logs if Number of Arguments is less than 4 ie.3") {
     val input = Array("aa", "bb", "bb")
     assert(StreamingTweetsJob.validateConfiguraiton(input) === true)
   }
 
-  test("Incorrect Number of Arguments for Main2") {
+  test("Should Fail the Validation with Error Logs if Number of Arguments is less than 4 ie.2") {
     val input = Array("aa", "bb")
     assert(StreamingTweetsJob.validateConfiguraiton(input) === true)
   }
 
-  test("Incorrect Number of Arguments for Main3") {
+  test("Should Fail the Validation with Error Logs if Number of Arguments is less than 4 ie. 1") {
     val input = Array("aa")
     assert(StreamingTweetsJob.validateConfiguraiton(input) === true)
   }
-
 }
