331e45ef1e57577787c9bf8c983a89aebfd75b4c
shSachdeva <ssachdeva@griddynamics.com>
2018-08-31 17:23:13 -0700

    Remove HardCoded Filters
    * Hardcoded filters for tweets are removed and code is enabled to take filters (if any) from main argument configurations.

--- src/main/scala/com/gd/twitterstreamingtToDatalake/TweetsIngestion.scala
+++ src/main/scala/com/gd/twitterstreamingtToDatalake/TweetsIngestion.scala
@@ -1,9 +1,7 @@
 package com.gd.twitterstreamingtToDatalake
 
 import com.gd.twitterstreamingtToDatalake.StreamingTweetsJob.ssc
-import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.twitter.TwitterUtils
-import twitter4j.Status
 
 object TweetsIngestion {
 
@@ -13,17 +11,13 @@ object TweetsIngestion {
     System.setProperty("twitter4j.oauth.consumerSecret", arr(1))
     System.setProperty("twitter4j.oauth.accessToken", arr(2))
     System.setProperty("twitter4j.oauth.accessTokenSecret", arr(3))
-
   }
 
-  def getTweets = {
+  def getTweets(filters:Array[String]) = {
 
-    val filters: Seq[String] = Seq("uscis")
     val tweets = TwitterUtils.createStream(ssc, None, filters)
-    val englishTweets = tweets.filter(_.getLang() == "en")
+    val englishTweets = tweets.filter(_.getLang == "en")
 
     englishTweets
-
   }
-
 }
