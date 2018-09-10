4cb7e680f1eed2988d7c955e34a7418023efa274
shSachdeva <ssachdeva@griddynamics.com>
2018-09-10 10:21:18 -0700

    Keep Code Style Consistent
    * Test case names are updated with only first word uppercase.

--- src/test/com/gd/twitterstreamingtToDatalake/ConfigValidationTest.scala
+++ src/test/com/gd/twitterstreamingtToDatalake/ConfigValidationTest.scala
@@ -4,22 +4,22 @@ import org.scalatest.FunSuite
 
 class ConfigValidationTest extends FunSuite {
 
-  test("Should Validate Configuration if Number of Arguments is four or more") {
+  test("Should validate configuration if number of arguments is four or more") {
     val input = Array("aa", "bb", "bb", "cc")
     assert(StreamingTweetsJob.validateConfiguraiton(input) === false)
   }
 
-  test("Should Fail the Validation with Error Logs if Number of Arguments is less than 4 ie.3") {
+  test("Should fail the validation with error Logs if number of arguments is less than 4 ie.3") {
     val input = Array("aa", "bb", "bb")
     assert(StreamingTweetsJob.validateConfiguraiton(input) === true)
   }
 
-  test("Should Fail the Validation with Error Logs if Number of Arguments is less than 4 ie.2") {
+  test("Should fail the validation with error logs if number of arguments is less than 4 ie.2") {
     val input = Array("aa", "bb")
     assert(StreamingTweetsJob.validateConfiguraiton(input) === true)
   }
 
-  test("Should Fail the Validation with Error Logs if Number of Arguments is less than 4 ie. 1") {
+  test("Should fail the validation with error Logs if number of arguments is less than 4 ie. 1") {
     val input = Array("aa")
     assert(StreamingTweetsJob.validateConfiguraiton(input) === true)
   }
