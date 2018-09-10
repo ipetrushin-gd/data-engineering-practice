106b8cbe3392d25d248c7c9bb74b453278d4adfc
shSachdeva <ssachdeva@griddynamics.com>
2018-09-10 10:30:06 -0700

    Add validation for Twitter keys Content
    * Function validateTwitterKeys is added to verify, if the content of twitter keys is acceptable or not.
    
    * Function argument name is updated to a meaningful one.

--- src/main/scala/com/gd/twitterstreamingtToDatalake/StreamingTweetsJob.scala
+++ src/main/scala/com/gd/twitterstreamingtToDatalake/StreamingTweetsJob.scala
@@ -4,38 +4,46 @@ import org.apache.log4j.Logger
 
 object StreamingTweetsJob extends SparkSessionWrapper {
 
-  //Set the Logger Level
   val log = Logger.getLogger(StreamingTweetsJob.getClass.getName)
 
   def main(args: Array[String]): Unit = {
 
-    if (validateConfiguration(args)) {
+    if (isConfValid(args)) {
+      val authorizationKeys = args.take(4)
+      TweetsIngestion.configureTwitter(authorizationKeys)
+
+      //Incase Only tweets related to a specific Hashtags are needed..
+      val filters = args.takeRight(args.length - 4)
+
+      val englishTweets = TweetsIngestion.getTweets(filters)
+      val text = TransformTweets.getText(englishTweets)
+      val hashTags = TransformTweets.getHashTags(text)
+
+      hashTags.saveAsTextFiles("tweets", "json")
+
+      ssc.start()
+      ssc.awaitTermination()
+    }
+    else
       System.exit(1)
-    }
-    val arr = args.take(4)
-    TweetsIngestion.configureTwitter(arr)
-
-    //Incase Only tweets related to a specific Hashtags are needed..
-    val filters = args.takeRight(args.length - 4)
-
-    val englishTweets = TweetsIngestion.getTweets(filters)
-    val text = TransformTweets.getText(englishTweets)
-    val hashTags = TransformTweets.getHashTags(text)
-
-    hashTags.saveAsTextFiles("tweets", "json")
-
-    ssc.start()
-    ssc.awaitTermination()
   }
 
-  def validateConfiguration(args: Array[String]): Boolean = {
+  def isConfValid(keys: Array[String]): Boolean = {
 
-    if (args.length < 4) {
-      log.error("Please login with Twitter Keys !")
-      log.error("Usage: TwitterData <ConsumerKey><ConsumerSecret><accessToken><accessTokenSecret>" +
-        "[<filters>]")
-      true
+    if (keys.length < 4) {
+      log.error("Number of keys should be at least four! Please login with all keys !")
+      false
     }
-    else false
+    else if (validateTwitterKeys(keys)) true
+    else {
+      log.error("Twitter keys are not correct !")
+      log.error("Usage: TwitterData <ConsumerKey> <ConsumerSecret> <accessToken> <accessTokenSecret> " +
+        "[<filters (If Any)>]")
+      false
+    }
   }
+
+    def validateTwitterKeys(keys:Array[String]):Boolean = {
+      keys.forall(_.contains(""))
+    }
 }
