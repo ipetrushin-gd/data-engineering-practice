fe1250c0e775071a8bb188c535949e7d79c716b4
shSachdeva <ssachdeva@griddynamics.com>
2018-09-10 10:58:30 -0700

    Add testcase for Twitter keys pattern

--- src/test/com/gd/twitterstreamingtToDatalake/ConfigValidationTest.scala
+++ src/test/com/gd/twitterstreamingtToDatalake/ConfigValidationTest.scala
@@ -5,22 +5,27 @@ import org.scalatest.FunSuite
 class ConfigValidationTest extends FunSuite {
 
   test("Should validate configuration if number of arguments is four or more") {
-    val input = Array("aa", "bb", "bb", "cc")
-    assert(StreamingTweetsJob.validateConfiguraiton(input) === false)
+    val input = Array("aa", "bb", "cc", "dd")
+    assert(StreamingTweetsJob.isConfValid(input) === true)
   }
 
   test("Should fail the validation with error Logs if number of arguments is less than 4 ie.3") {
     val input = Array("aa", "bb", "bb")
-    assert(StreamingTweetsJob.validateConfiguraiton(input) === true)
+    assert(StreamingTweetsJob.isConfValid(input) === false)
   }
 
   test("Should fail the validation with error logs if number of arguments is less than 4 ie.2") {
     val input = Array("aa", "bb")
-    assert(StreamingTweetsJob.validateConfiguraiton(input) === true)
+    assert(StreamingTweetsJob.isConfValid(input) === false)
   }
 
   test("Should fail the validation with error Logs if number of arguments is less than 4 ie. 1") {
     val input = Array("aa")
-    assert(StreamingTweetsJob.validateConfiguraiton(input) === true)
+    assert(StreamingTweetsJob.isConfValid(input) === false)
+  }
+
+  test("Should fail the validation with error logs if all the four arguments do not contain the pattern") {
+    val input = Array("","","","")
+    assert(StreamingTweetsJob.validateTwitterKeys(input) === true)
   }
 }
