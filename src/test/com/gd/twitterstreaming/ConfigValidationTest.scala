889041807941b6f91be8a8b8a9976f19946b084a
shSachdeva <ssachdeva@griddynamics.com>
2018-09-12 10:35:30 -0700

    Update TestCase name

--- src/test/com/gd/twitterstreaming/ConfigValidationTest.scala
+++ src/test/com/gd/twitterstreaming/ConfigValidationTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FreeSpec
 
 class ConfigValidationTest extends FreeSpec {
 
-  "ConfigValidator"- {
+  "ConfigValidator isConfValid function"- {
     "when invoked with 4 or more arguments"- {
       "should pass validation" in {
         val input = Array("aa", "bb", "cc", "dd")
@@ -12,19 +12,25 @@ class ConfigValidationTest extends FreeSpec {
       }
     }
     "when invoked with less than 4 arguments that is 3"- {
-      "should fail vaildation with error logs" in {
+      "should fail vaildation" in {
         val input = Array("aa", "bb", "bb")
         assert(ConfigValidator.isConfValid(input) === false)
       }
-      "should fail vaildation with error logs that is 2" in {
+    }
+    "when invoked with less than 4 arguments that is 2"- {
+      "should fail vaildation" in {
         val input = Array("aa", "bb")
         assert(ConfigValidator.isConfValid(input) === false)
       }
-      "should fail vaildation with error logs that is 1" in {
+    }
+    "when invoked with less than 4 arguments that is 1"- {
+      "should fail vaildation" in {
         val input = Array("aa")
         assert(ConfigValidator.isConfValid(input) === false)
       }
-      "should fail vaildation with error logs that is none" in {
+    }
+    "when invoked with less than 4 arguments that is None"- {
+      "should fail vaildation" in {
         val input = Array[String]()
         assert(ConfigValidator.isConfValid(input) === false)
       }
