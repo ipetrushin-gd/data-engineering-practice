929a5d5d16c32a203a354d22726cdf675aa82ba3
shSachdeva <ssachdeva@griddynamics.com>
2018-09-13 16:14:09 -0700

    Add a new File for Configurable Values

---
+++ src/main/scala/com/gd/twitterstreaming/util/TweetsConstant.scala
@@ -0,0 +1,11 @@
+package com.gd.twitterstreaming.util
+
+object TweetsConstant {
+
+  val PARTITION_COLUMN = "event_date"
+  val SAVEPATH = "twitter-analytics/dataLake/eng-tweets"
+  val TABLENAME = "engtweets"
+  val OUTPUT_DATE_FORMAT = "YYYYMMdd HH.mm.ss"
+  val TWEETS_LANG_FILTER = "en"
+
+}
