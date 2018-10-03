b88cf6617c9b056f19e120e04e95f13c3fabcb9b
shSachdeva <ssachdeva@griddynamics.com>
2018-10-02 17:41:23 -0700

    Add returntypes to functions

--- src/main/scala/com/gd/twitteranalytics/TweetsTransformer.scala
+++ src/main/scala/com/gd/twitteranalytics/TweetsTransformer.scala
@@ -2,7 +2,6 @@ package com.gd.twitteranalytics
 
 import java.util.{Date => JavaDate}
 import java.sql.Timestamp
-import com.gd.twitteranalytics.util.AppConfigReader
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions.date_format
@@ -11,20 +10,19 @@ import org.apache.spark.rdd.RDD
 
 object TweetsTransformer{
 
-  def getDateAndText(englishTweets: DStream[Status]) = {
+  def getDateAndText(englishTweets: DStream[Status]): DStream[(JavaDate,String)] = {
     englishTweets.map(x => (x.getCreatedAt,x.getText))
   }
 
-  def convertRddToDataFrame(pairRdd:RDD[(JavaDate,String)])= {
+  def convertRddToDataFrame(pairRdd:RDD[(JavaDate,String)]) : DataFrame= {
     val spark = SparkSession.builder.config(pairRdd.sparkContext.getConf).getOrCreate
     import spark.implicits._
 
     pairRdd.map(tweets => Tweet(new Timestamp(tweets._1.getTime),tweets._2)).toDF
   }
 
-  def updateDateColFormat(inputDataFrame:DataFrame,dateCol:String) = {
-    val updatedDateFormat = date_format(inputDataFrame(dateCol),
-                                                      AppConfigReader.OutputDateFormat)
+  def updateDateColFormat(inputDataFrame:DataFrame,dateCol:String) : DataFrame = {
+    val updatedDateFormat = date_format(inputDataFrame(dateCol),"YYYYMMdd")
     inputDataFrame.withColumn(dateCol,updatedDateFormat)
   }
 }
