2e78fb5e01154e3098db7bd5a1a921330ea3c6fe
shSachdeva <ssachdeva@griddynamics.com>
2018-10-05 15:51:16 -0700

    Reformat Transform Functions
    * convertDataFrameToDataSet function is added to convert Dataframe to DataSet[Tweet]
    
    * Column type of Dataframe is updated from timestamp to java.sql.date

--- src/main/scala/com/gd/twitteranalytics/TweetsTransformer.scala
+++ src/main/scala/com/gd/twitteranalytics/TweetsTransformer.scala
@@ -1,12 +1,12 @@
 package com.gd.twitteranalytics
 
 import java.util.{Date => JavaDate}
-import java.sql.Timestamp
+import java.sql.Date
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.spark.streaming.dstream.DStream
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions.date_format
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.functions.to_date
 import twitter4j.Status
 import org.apache.spark.rdd.RDD
 
@@ -17,15 +17,20 @@ object TweetsTransformer{
     englishTweets.map(x => (x.getCreatedAt,mapper.writeValueAsString(x)))
   }
 
-  def convertRddToDataFrame(pairRdd:RDD[(JavaDate,String)]) : DataFrame= {
+  def convertRddToDataFrame(pairRdd:RDD[(JavaDate,String)]) : DataFrame = {
     val spark = SparkSession.builder.config(pairRdd.sparkContext.getConf).getOrCreate
     import spark.implicits._
 
-    pairRdd.map(tweets => Tweet(new Timestamp(tweets._1.getTime),tweets._2)).toDF
+    pairRdd.map(tweets => Tweet(new Date(tweets._1.getTime),tweets._2)).toDF
   }
 
-  def updateDateColFormat(inputDataFrame:DataFrame,dateCol:String) : DataFrame = {
-    val updatedDateFormat = date_format(inputDataFrame(dateCol),"YYYYMMdd")
+  def updateDateColFormat(inputDataFrame:DataFrame,dateCol:String): DataFrame = {
+    val updatedDateFormat = to_date(inputDataFrame(dateCol),"yyyy-MM-dd")
     inputDataFrame.withColumn(dateCol,updatedDateFormat)
+  }
+
+  def convertDataFrameToDataSet(inputDataFrame:DataFrame): Dataset[Tweet] = {
+    val encoder = org.apache.spark.sql.Encoders.product[Tweet]
+    inputDataFrame.as[Tweet](encoder)
   }
 }
