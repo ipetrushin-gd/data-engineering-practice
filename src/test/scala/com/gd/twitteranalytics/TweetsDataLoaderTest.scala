0b484282a5536a27392df1f84fd45e3d66b6c4a0
shSachdeva <ssachdeva@griddynamics.com>
2018-10-05 16:11:49 -0700

    Update verification of saveOutputToHdfs function
    * Testcase is added to assert that files created when saveOutputToHdfs is called are in parquet format and content is same as sent to saveOutputToHdfs.
    
    * Testcase is updated to verify with argument of Dataset with Column type Date instead of String

--- src/test/scala/com/gd/twitteranalytics/TweetsDataLoaderTest.scala
+++ src/test/scala/com/gd/twitteranalytics/TweetsDataLoaderTest.scala
@@ -1,25 +1,46 @@
 package com.gd.twitteranalytics
 
 import java.io.File
+import java.util.{Date => JavaDate}
+import java.sql.Date
+
 import com.gd.twitteranalytics.util.AppConfigReader
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.scalatest.FreeSpec
 
 class TweetsDataLoaderTest extends FreeSpec with DataFrameSuiteBase {
 
-  "Methods tests" - {
-    "saveOutputToHdfs when presented with dataframe, savePath and partitionColumn" - {
-      "should create event_datetime folder structure in fileSystem at given path" in {
-        val text = "This is the #Sample #text for #test purpose"
-        val event_date = "20180928"
-        val savePath = AppConfigReader.getAppConfigurables(0)
+  "TweetsDataLoader" - {
+    "saveOutputToHdfs function when presented with Dataset[Tweet] and savePath" - {
+      "should create event_date=YYYY-MM-DD folder structure in fileSystem at given path" in {
+        val payload = "This is the #Sample #text for #test purpose"
+        val event_date = new Date(new JavaDate("Wed Sep 26 10:37:20 PDT 2018").getTime)
+        val sampleDate = "2018-09-26"
         val sqlCtx = sqlContext
         import sqlCtx.implicits._
 
-        val sourceDf = Seq((event_date,text),(event_date,text)).toDF("event_date","payload")
-        TweetsDataLoader.saveOutputToHdfs(sourceDf,savePath)
-        val file = new File(savePath+"/event_date="+s"${event_date}")
-        assert(file.exists ===true)
+        val savePath = AppConfigReader.getAppConfigurables(0)
+        val tweetDataSet = Seq(Tweet(event_date,payload),Tweet(event_date,payload)).toDS
+
+        TweetsDataLoader.saveOutputToHdfs(tweetDataSet,savePath)
+        val file = new File(savePath+"/event_date="+s"${sampleDate}")
+        assert(file.exists === true)
+      }
+    }
+    "saveOutputToHdfs function when saves data on HDFS at savepath" - {
+      "it should be in parquet format and data content should be same as received" in {
+        val sqlCtx = sqlContext
+        import sqlCtx.implicits._
+
+        val payload = "This is the #Sample #text for #test purpose"
+        val event_date = new Date(new JavaDate("Wed Sep 26 10:37:20 PDT 2018").getTime)
+        val sampleDate = "2018-09-26"
+        val savePath = AppConfigReader.getAppConfigurables(0)
+        val tweetDataSet = Seq(Tweet(event_date,payload),Tweet(event_date,payload)).toDS
+
+        TweetsDataLoader.saveOutputToHdfs(tweetDataSet,savePath)
+        val dataFrameFromCreatedFolder = spark.read.parquet(savePath+"/event_date="+s"${sampleDate}")
+        assert(dataFrameFromCreatedFolder.select("payload").first().mkString === payload)
       }
     }
   }
