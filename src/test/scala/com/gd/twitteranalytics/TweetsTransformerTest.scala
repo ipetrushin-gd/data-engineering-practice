4f397be8cacebdc5fec5418ced3150fe02c85141
shSachdeva <ssachdeva@griddynamics.com>
2018-10-05 16:01:50 -0700

    Reformat testcases
    * Testcase is added to assert convertDataFrameToDataSet function.
    
    * All dataframes inside all testcases are created with Tweet type instead of generic types.
    
    * Testcase related to updateDateColFormat is updated to verify type of column as Date instead of String.

--- src/test/scala/com/gd/twitteranalytics/TweetsTransformerTest.scala
+++ src/test/scala/com/gd/twitteranalytics/TweetsTransformerTest.scala
@@ -1,13 +1,12 @@
 package twitteranalytics
 
-import java.sql.Timestamp
-import java.util.Date
+import java.sql.Date
+import java.util.{Date => JavaDate}
 
-import com.gd.twitteranalytics.TweetsTransformer
+import com.gd.twitteranalytics.{Tweet, TweetsTransformer}
 import com.holdenkarau.spark.testing.{DataFrameSuiteBase, StreamingSuiteBase}
 import org.scalatest.FreeSpec
 import twitter4j.TwitterObjectFactory
-
 import com.fasterxml.jackson.databind.ObjectMapper
 
 import scala.io.Source
@@ -15,9 +14,9 @@ import scala.io.Source
 class TweetsTransformerTest extends FreeSpec with StreamingSuiteBase with DataFrameSuiteBase {
 
   "TransformTweets" - {
-    val creationTime = new Date("Wed Sep 26 10:37:20 PDT 2018")
+    val creationTime = new JavaDate("Wed Sep 26 10:37:20 PDT 2018")
     val text = "This is the #Sample #text for #test purpose"
-    val eventDate = new Timestamp(creationTime.getTime)
+    val eventDate = new Date(creationTime.getTime)
 
     "getCreationDateAndStatus function" - {
     "when presented with a complete tweet status" - {
@@ -38,7 +37,7 @@ class TweetsTransformerTest extends FreeSpec with StreamingSuiteBase with DataFr
           val sqlCtx = sqlContext
           import sqlCtx.implicits._
 
-          val expectedDf = Seq((eventDate,text),(eventDate,text)).toDF("event_date","payload")
+          val expectedDf = Seq(Tweet(eventDate,text),Tweet(eventDate,text)).toDF("event_date","payload")
           val inputRdd = sc.parallelize(Seq((creationTime, text), (creationTime, text)))
           val generatedDf = TweetsTransformer.convertRddToDataFrame(inputRdd)
           assertDataFrameEquals(expectedDf,generatedDf)
@@ -47,14 +46,26 @@ class TweetsTransformerTest extends FreeSpec with StreamingSuiteBase with DataFr
     }
     "updateDateColFormat function" - {
       "when presented with a DataFrame of format YYYY-MM-DD HH:MM:SS" -{
-        "should update the format to YYYYMMDD" in {
+        "should update the format to YYYY-MM-DD" in {
           val sqlCtx = sqlContext
           import sqlCtx.implicits._
 
-          val sourceDf = Seq((eventDate,text),(eventDate,text)).toDF("event_date","text")
+          val sourceDf = Seq(Tweet(eventDate,text),Tweet(eventDate,text)).toDF("event_date","payload")
           val generatedDf = TweetsTransformer.updateDateColFormat(sourceDf,"event_date")
-          val dateFromDf = generatedDf.select("event_date").first.getString(0)
-          assert("20180926" === dateFromDf)
+          val dateFromDf = generatedDf.select("event_date").first.getAs[Date](0)
+          assert(Date.valueOf("2018-09-26") === dateFromDf)
+        }
+      }
+    }
+    "convertDataFrameToDataSet function" - {
+      "when presented with a Dataframe with Columns eventDate and text" - {
+        "should produce a DataFrame of type Tweet" in {
+          val sqlCtx = sqlContext
+          import sqlCtx.implicits._
+
+          val sourceDf = Seq(Tweet(eventDate,text),Tweet(eventDate,text)).toDF
+          val generatedDataSet = TweetsTransformer.convertDataFrameToDataSet(sourceDf)
+          assert(sourceDf.schema === generatedDataSet.schema)
         }
       }
     }
