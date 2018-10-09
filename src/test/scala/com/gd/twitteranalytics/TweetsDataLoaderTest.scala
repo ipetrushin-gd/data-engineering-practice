8d7636f794b42256c478f4ead4820b02042b6ac4
shSachdeva <ssachdeva@griddynamics.com>
2018-10-09 14:36:50 -0700

    Add Cleanup to delete data generated in testcases
    BeforeAndAfter Trait is added to have a created Dataframe in Before{} whose path can be used in every testcase and then delete the created Directory in After {}.

--- src/test/scala/com/gd/twitteranalytics/TweetsDataLoaderTest.scala
+++ src/test/scala/com/gd/twitteranalytics/TweetsDataLoaderTest.scala
@@ -1,46 +1,44 @@
 package com.gd.twitteranalytics
 
-import java.io.File
 import java.util.{Date => JavaDate}
 import java.sql.Date
 
 import com.gd.twitteranalytics.util.AppConfigReader
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
-import org.scalatest.FreeSpec
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.scalatest.{BeforeAndAfter, FreeSpec}
 
-class TweetsDataLoaderTest extends FreeSpec with DataFrameSuiteBase {
+class TweetsDataLoaderTest extends FreeSpec with DataFrameSuiteBase with BeforeAndAfter{
+
+    val payload = "This is the #Sample #text for #test purpose"
+    val event_date = new Date(new JavaDate("Wed Sep 26 10:37:20 PDT 2018").getTime)
+    val sampleDate = "2018-09-26"
+    val savePath = AppConfigReader.getAppConfigurables(0)+"/event_date="+s"${sampleDate}"
+
+  before{
+    val sqlCtx = sqlContext
+    import sqlCtx.implicits._
+
+    val tweetDataSet = Seq(Tweet(event_date,payload),Tweet(event_date,payload)).toDS
+    TweetsDataLoader.saveOutputToHdfs(tweetDataSet,savePath)
+  }
+
+  after {
+    val fs = FileSystem.get(sc.hadoopConfiguration)
+    fs.delete(new Path(savePath),true)
+  }
 
   "TweetsDataLoader" - {
     "saveOutputToHdfs function when presented with Dataset[Tweet] and savePath" - {
       "should create event_date=YYYY-MM-DD folder structure in fileSystem at given path" in {
-        val payload = "This is the #Sample #text for #test purpose"
-        val event_date = new Date(new JavaDate("Wed Sep 26 10:37:20 PDT 2018").getTime)
-        val sampleDate = "2018-09-26"
-        val sqlCtx = sqlContext
-        import sqlCtx.implicits._
-
-        val savePath = AppConfigReader.getAppConfigurables(0)
-        val tweetDataSet = Seq(Tweet(event_date,payload),Tweet(event_date,payload)).toDS
-
-        TweetsDataLoader.saveOutputToHdfs(tweetDataSet,savePath)
-        val file = new File(savePath+"/event_date="+s"${sampleDate}")
-        assert(file.exists === true)
+        val fs = FileSystem.get(sc.hadoopConfiguration)
+        assert (fs.exists(new Path(savePath)) === true)
       }
     }
     "saveOutputToHdfs function when saves data on HDFS at savepath" - {
       "it should be in parquet format and data content should be same as received" in {
-        val sqlCtx = sqlContext
-        import sqlCtx.implicits._
-
-        val payload = "This is the #Sample #text for #test purpose"
-        val event_date = new Date(new JavaDate("Wed Sep 26 10:37:20 PDT 2018").getTime)
-        val sampleDate = "2018-09-26"
-        val savePath = AppConfigReader.getAppConfigurables(0)
-        val tweetDataSet = Seq(Tweet(event_date,payload),Tweet(event_date,payload)).toDS
-
-        TweetsDataLoader.saveOutputToHdfs(tweetDataSet,savePath)
-        val dataFrameFromCreatedFolder = spark.read.parquet(savePath+"/event_date="+s"${sampleDate}")
-        assert(dataFrameFromCreatedFolder.select("payload").first().mkString === payload)
+        val dataFromHdfsFile = spark.read.parquet(savePath)
+        assert(dataFromHdfsFile.select("payload").first().mkString === payload)
       }
     }
   }
