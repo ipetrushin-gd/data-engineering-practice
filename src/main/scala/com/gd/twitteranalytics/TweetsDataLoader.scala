b1869e5485f4e70b0a846061d1b2ae540e5ff63e
shSachdeva <ssachdeva@griddynamics.com>
2018-10-22 10:58:40 -0700

    Add function to save reports on HDFS

--- src/main/scala/com/gd/twitteranalytics/TweetsDataLoader.scala
+++ src/main/scala/com/gd/twitteranalytics/TweetsDataLoader.scala
@@ -1,11 +1,16 @@
 package com.gd.twitteranalytics
 
-import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.{DataFrame, Dataset}
 
 object TweetsDataLoader {
 
   def saveOutputToHdfs(inputDataSet: Dataset[Tweet],savePath:String)={
     inputDataSet.write.partitionBy("event_date").
       format("parquet").mode("append").save(savePath)
+  }
+
+  def saveReportToHdfs(inputDataSet: DataFrame, savePath:String)={
+    inputDataSet.write.partitionBy("Date").
+      format("csv").mode("append").save(savePath)
   }
 }
