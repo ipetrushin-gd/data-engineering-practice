9a282a783a812485b1581188c2467d7ca3c9c813
shSachdeva <ssachdeva@griddynamics.com>
2018-10-22 10:57:23 -0700

    Generate Report for Twitter Analytics
    Twitter report is generated with 3 different versions from
    * SQL
    * DataFrame
    * DataSet API

---
+++ src/main/scala/com/gd/twitteranalytics/TwitterReportGenerator.scala
@@ -0,0 +1,40 @@
+package com.gd.twitteranalytics
+
+import com.gd.twitteranalytics.TwitterReportingJob.log
+import org.apache.spark.sql.functions.count
+import org.apache.spark.sql.{DataFrame, Dataset, Encoders, SparkSession}
+import org.apache.spark.sql.functions._
+
+object TwitterReportGenerator {
+
+  def getReportWithSqlProcessing(spark:SparkSession,tweetStatusDf:DataFrame):DataFrame = {
+    log.debug("=======> Stage 3: Twitter Report via SQL Start.... ")
+    tweetStatusDf.createOrReplaceTempView("status")
+    val query = "select location,id,count(id) as frequency from status group by location,id having frequency > 10"
+    spark.sql(query).withColumn("Date",lit(current_date))
+  }
+
+  def getReportWithDataFrameProcessing(spark:SparkSession,tweetStatusDf:DataFrame):DataFrame = {
+    log.debug("=======> Stage 4: Twitter Report via SQL Start.... ")
+    import spark.implicits._
+
+    tweetStatusDf.select($"location",$"id").
+                  groupBy("location","id") .
+                  agg(count($"id")as "frequency").where(count($"id")>10).
+                  withColumn("Date",lit(current_date))
+  }
+
+  def getReportWithDataSetProcessing(spark:SparkSession,tweetStatusDf:DataFrame):DataFrame = {
+    log.debug("=======> Stage 5: Twitter Report via SQL Start.... ")
+    import spark.implicits._
+
+    val encoder = Encoders.product[TwitterStatus]
+    val tweetDs:Dataset[TwitterStatus] = tweetStatusDf.as[TwitterStatus](encoder)
+        tweetDs.select($"location",$"id").
+                groupBy("location","id") .
+                agg(count($"id")as "frequency").where(count($"id")>10).
+                withColumn("Date",lit(current_date))
+  }
+}
+
+case class TwitterStatus(location:String,id:Long)
