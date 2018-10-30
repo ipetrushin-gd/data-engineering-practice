effa299fc59a05dafa1fd405dab3880bf934f0e3
shSachdeva <ssachdeva@griddynamics.com>
2018-10-30 15:24:10 -0700

    Add code for report generation through Dataset typesafe manner
    Report creation via Dataset implementation that was causing issue due to encoder has been fixed.

--- src/main/scala/com/gd/twitteranalytics/TwitterReportGenerator.scala
+++ src/main/scala/com/gd/twitteranalytics/TwitterReportGenerator.scala
@@ -1,58 +1,49 @@
 package com.gd.twitteranalytics
 
-import com.gd.twitteranalytics.TwitterReportingJob.log
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.count
-import org.apache.spark.sql.{DataFrame, Dataset, Encoders, SparkSession}
+import org.apache.spark.sql.{DataFrame,Dataset,Encoders,SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.scalalang.typed.{count => typedCount}
 
 object TwitterReportGenerator {
 
-  def getReportWithSqlProcessing(spark:SparkSession,tweetStatusDf:DataFrame):DataFrame = {
-    log.debug("=======> Stage 3: Twitter Report via SQL Start.... ")
+  def getReportWithSqlProcessing(spark: SparkSession, tweetStatusDf: DataFrame): DataFrame = {
     tweetStatusDf.createOrReplaceTempView("status")
     val query = "select location,id,count(id) as frequency from status group by location,id having frequency >= 10"
-    spark.sql(query).withColumn("Date",lit(current_date)).createOrReplaceTempView("filteredTable")
+    spark.sql(query).withColumn("Date", lit(current_date)).createOrReplaceTempView("filteredTable")
 
     spark.sql("select location, id, Date from (select *, row_number() " +
       "OVER (PARTITION BY location ORDER BY frequency DESC) as rn  FROM filteredTable) tmp where rn <= 5")
   }
 
-  def getReportWithDataFrameProcessing(spark:SparkSession,tweetStatusDf:DataFrame):DataFrame = {
-    log.debug("=======> Stage 4: Twitter Report via SQL Start.... ")
+  def getReportWithDataFrameProcessing(spark: SparkSession, tweetStatusDf: DataFrame): DataFrame = {
     import spark.implicits._
 
-    val aggregatedDF  = tweetStatusDf.select($"location",$"id").
-                  groupBy("location","id") .
-                  agg(count($"id")as "frequency").where(count($"id") geq 10).
-                  withColumn("Date",lit(current_date))
+    val aggregatedDF = tweetStatusDf.select($"location", $"id").
+      groupBy("location", "id").
+      agg(count($"id") as "frequency").where(count($"id") geq 10).
+      withColumn("Date", lit(current_date))
 
     val byBucket = Window.partitionBy($"location").orderBy($"frequency".desc)
     aggregatedDF.withColumn("rn", row_number.over(byBucket)).
-            where($"rn" <= 5).drop("rn").drop("frequency")
+                      where($"rn" <= 5).drop("rn").drop("frequency")
   }
 
-  def getReportWithDataSetProcessing(spark:SparkSession,tweetStatusDf:DataFrame):DataFrame = {
-    log.debug("=======> Stage 5: Twitter Report via SQL Start.... ")
+  def getReportWithDataSetProcessing1(spark: SparkSession, tweetStatusDf: DataFrame) = {
     import spark.implicits._
+    implicit val encoderForStatus = Encoders.product[TwitterStatus]
+    implicit val encoderForReport = Encoders.product[Report]
+    val tweetDs: Dataset[TwitterStatus] = tweetStatusDf.as[TwitterStatus](encoderForStatus)
 
-    val encoder = Encoders.product[TwitterStatus]
-    val tweetDs:Dataset[TwitterStatus] = tweetStatusDf.as[TwitterStatus](encoder)
-    val aggregatedDF = tweetDs.groupByKey(x=> TwitterStatus(x.location,x.id))
-                        .agg(typedCount[TwitterStatus](_.id).name("frequency"))
-                        .where($"frequency" geq 10)
-                        .map{case( (TwitterStatus(location, id)),frequency) => Report(location,id,frequency)}
+    val aggregatedDF = tweetDs.groupByKey(x => TwitterStatus(x.location, x.id))
+                              .agg(typedCount[TwitterStatus](_.id).name("frequency"))
+                              .where($"frequency" geq 10)
+                              .map { case (TwitterStatus(location, id), frequency) => Report(location, id, frequency) }
 
-    val byBucket = Window.partitionBy($"location").orderBy($"frequency".desc)
-
-    // This is attempt to get report via Typed Dataset --> But getting error Unable to find encoder for type stored in Dataset
-   /* val reportViaTypedDataset = sortedDf.groupByKey(_.location).mapValues(List(_)).mapValues(_.sortBy(_.frequency).take(5)).
-      mapGroups{case(k,iter)=> iter.map(x => x.toDF.as[Report])}*/
-
-    aggregatedDF.withColumn("Date",lit(current_date)).
-              withColumn("rn",row_number.over(byBucket)).
-              where($"rn" <=5).drop("rn").drop("frequency")
+    aggregatedDF.groupByKey(_.location)
+      .mapValues(List(_)).mapValues(_.sortBy(x => -x.frequency).take(5))
+      .flatMapGroups{case(value,iter) => iter.flatten}.toDF
   }
 }
 
