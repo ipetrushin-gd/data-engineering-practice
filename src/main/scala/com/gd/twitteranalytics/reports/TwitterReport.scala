834e01109b24d14152c7e7d3d9943ca0cb7d26d9
shSachdeva <ssachdeva@griddynamics.com>
2018-10-31 15:23:56 -0700

    Add Trait TwitterReport for ActiveUser reports of 3 versions

---
+++ src/main/scala/com/gd/twitteranalytics/reports/TwitterReport.scala
@@ -0,0 +1,25 @@
+package com.gd.twitteranalytics.reports
+
+import com.gd.twitteranalytics.util.AppConfigReader
+import org.apache.log4j.Logger
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+trait TwitterReport {
+
+  val today = java.time.LocalDate.now
+  val dataPath = AppConfigReader.getAppConfigurables(0) + "/event_date=" + today
+  val reportSavePath = AppConfigReader.getAppConfigurables(3) + "/ActiveUsers"
+
+  def getInputDataForReport(dataPath:String,spark:SparkSession):DataFrame = {
+    val tweetsDataFrame = ReportInputDataParser.getPayloadStatusAsDataset(spark, dataPath)
+    ReportInputDataParser.getUserIdAndLocation(spark, tweetsDataFrame)
+  }
+
+  def getLogger(className:String):Logger = {
+    Logger.getLogger(className.getClass.getName)
+  }
+
+  def setSparkSession(name:String):SparkSession = {
+    SparkSession.builder.appName(name).master("local[*]")getOrCreate
+  }
+}
