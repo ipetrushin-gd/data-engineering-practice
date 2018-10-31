17282232829979056fa5745f1135c9c67c6cf66c
shSachdeva <ssachdeva@griddynamics.com>
2018-10-30 17:56:14 -0700

    Add Strategy to pass Report processing Alogs
    Execute function is added where report processing algorithms can be passed instead of having different algos to be called
    
    Move report processing Algos to ReportProcessor

--- src/main/scala/com/gd/twitteranalytics/TwitterReportGenerator.scala
+++ src/main/scala/com/gd/twitteranalytics/TwitterReportGenerator.scala
@@ -4,6 +4,8 @@ import org.apache.spark.sql.{DataFrame,SparkSession}
 
 object TwitterReportGenerator {
 
+  def execute(f: (SparkSession, DataFrame) => DataFrame, spark: SparkSession, tweetStatusDf: DataFrame) = f(spark, tweetStatusDf)
+
   def execute(reportProcessor: (SparkSession, DataFrame) => DataFrame,
               spark: SparkSession, tweetStatusDf: DataFrame) = reportProcessor(spark, tweetStatusDf)
 }
