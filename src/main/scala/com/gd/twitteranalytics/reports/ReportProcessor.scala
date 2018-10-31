fee093fc41bcefb466884559cff1ed6f9e301b08
shSachdeva <ssachdeva@griddynamics.com>
2018-10-31 15:27:15 -0700

    Update Variable names to imply Logic

--- src/main/scala/com/gd/twitteranalytics/reports/ReportProcessor.scala
+++ src/main/scala/com/gd/twitteranalytics/reports/ReportProcessor.scala
@@ -9,23 +9,23 @@ object ReportProcessor {
 
   def getReportWithSqlProcessing(spark: SparkSession, tweetStatusDf: DataFrame): DataFrame = {
     tweetStatusDf.createOrReplaceTempView("status")
-    val query = "select location,id,count(id) as frequency from status group by location,id having frequency >= 10"
-    spark.sql(query).withColumn("Date", lit(current_date)).createOrReplaceTempView("filteredTable")
+    val activeUsersIdAndLocation = "select location,id,count(id) as frequency from status group by location,id having frequency >= 10"
+    spark.sql(activeUsersIdAndLocation).withColumn("Date", lit(current_date)).createOrReplaceTempView("activeUsers")
 
     spark.sql("""select location, id, Date from (select *, row_number()
-      OVER (PARTITION BY location ORDER BY frequency DESC) as rn  FROM filteredTable) tmp where rn <= 5""")
+      OVER (PARTITION BY location ORDER BY frequency DESC) as rn  FROM activeUsers) groupSortedByFreq where rn <= 5""")
   }
 
   def getReportWithDataFrameProcessing(spark: SparkSession, tweetStatusDf: DataFrame): DataFrame = {
     import spark.implicits._
 
-    val aggregatedDF = tweetStatusDf.select($"location", $"id").
+    val activeUsers = tweetStatusDf.select($"location", $"id").
       groupBy("location", "id").
       agg(count($"id") as "frequency").where(count($"id") geq 10).
       withColumn("Date", lit(current_date))
 
     val byBucket = Window.partitionBy($"location").orderBy($"frequency".desc)
-    aggregatedDF.withColumn("rn", row_number.over(byBucket)).
+    activeUsers.withColumn("rn", row_number.over(byBucket)).
       where($"rn" <= 5).drop("rn").drop("frequency")
   }
 
@@ -35,17 +35,16 @@ object ReportProcessor {
     implicit val encoderForReport = Encoders.product[Report]
     val tweetDs: Dataset[TwitterStatus] = tweetStatusDf.as[TwitterStatus](encoderForStatus)
 
-    val aggregatedDF = tweetDs.groupByKey(x => TwitterStatus(x.location, x.id))
+    val activeUsers = tweetDs.groupByKey(x => TwitterStatus(x.location, x.id))
       .agg(typedCount[TwitterStatus](_.id).name("frequency"))
       .where($"frequency" geq 10)
       .map { case (TwitterStatus(location, id), frequency) => Report(location, id, frequency) }
 
-    aggregatedDF.groupByKey(_.location)
+    activeUsers.groupByKey(_.location)
       .mapValues(List(_)).mapValues(_.sortBy(x => -x.frequency).take(5))
       .flatMapGroups{case(value,iter) => iter.flatten}.
       drop("frequency").withColumn("Date", lit(current_date))
   }
 }
-
 case class TwitterStatus(location:String,id:Long)
 case class Report(location:String,id:Long,frequency:Long)
