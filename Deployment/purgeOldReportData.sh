a72f60412be24f9526b920d146eada2b6ad124f4
shSachdeva <ssachdeva@griddynamics.com>
2018-11-04 11:54:39 -0800

    Update config properties name to reflect corresponding report
    * Property names are updated to reflect that these properties relate to activeUsers report.
    * Related script file purgeOldReport.sh has been updated accordingly.

--- Deployment/purgeOldReportData.sh
+++ Deployment/purgeOldReportData.sh
@@ -13,10 +13,10 @@ echo "=====> Older Data is from date : $past_date"
 
 echo "=====> $current_date: INFO: Scanning reports Data for retention.."
 
-echo "=====> INFO: Purging reports older than $past_date from $report_path....."
+echo "=====> INFO: Purging reports older than $past_date from $activeUserReport_path....."
 
 today=`date +'%s'`
-hdfs dfs -ls $dfReportPath/ | grep "^d" | while read line ; do
+hdfs dfs -ls $activeUserDfReportPath/ | grep "^d" | while read line ; do
 dir_date=$(echo ${line} | awk '{print $6}')
 difference=$(( ( ${today} - $(date -d ${dir_date} +%s) ) / ( 24*60*60 ) ))
 filePath=$(echo ${line} | awk '{print $8}')
@@ -29,7 +29,7 @@ echo "No Data to Purge.."
 fi
 done
 
-hdfs dfs -ls $sqlReportPath/ | grep "^d" | while read line ; do
+hdfs dfs -ls $activeUserSqlReportPath/ | grep "^d" | while read line ; do
 dir_date=$(echo ${line} | awk '{print $6}')
 difference=$(( ( ${today} - $(date -d ${dir_date} +%s) ) / ( 24*60*60 ) ))
 filePath=$(echo ${line} | awk '{print $8}')
@@ -42,7 +42,7 @@ echo "No Data to Purge.."
 fi
 done
 
-hdfs dfs -ls $dsReportPath/ | grep "^d" | while read line ; do
+hdfs dfs -ls $activeUserDsReportPath/ | grep "^d" | while read line ; do
 dir_date=$(echo ${line} | awk '{print $6}')
 difference=$(( ( ${today} - $(date -d ${dir_date} +%s) ) / ( 24*60*60 ) ))
 filePath=$(echo ${line} | awk '{print $8}')
