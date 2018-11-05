ac484cabfba2aea02a13a29889b20269ca86cf78
shSachdeva <ssachdeva@griddynamics.com>
2018-11-05 10:15:55 -0800

    Remove common code from ActiveUser report drivers
    * Common code has been removed from report driver objects and moved into trait TwitterReport for re-usability purpose.

--- src/main/scala/com/gd/twitteranalytics/reports/ActiveUserDatasetReport.scala
+++ src/main/scala/com/gd/twitteranalytics/reports/ActiveUserDatasetReport.scala
@@ -2,30 +2,20 @@ package com.gd.twitteranalytics.reports
 
 import ActiveUserReportProcessor._
 import TwitterReportGenerator._
-import TwitterReportWriter._
-import org.apache.hadoop.fs.{FileSystem, Path}
 
 object ActiveUserDatasetReport extends TwitterReport{
   val log = getLogger("ActiveUserDataSetReport")
 
   def main(args: Array[String]): Unit = {
     val spark = setSparkSession("TwitterActiveUserDataSetReport")
-    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
-
-    if (fs.exists(new Path(dataPath))) {
-      log.debug("=======> Building Twitter ActiveUser Report for date " + today)
-      val inputDataForReport = getInputDataForReport(dataPath, spark)
-
-      log.debug("=======> Stage 1: Twitter Data Read Complete !")
-      log.debug("=======> Stage 2: Generating Report for Active Users..")
+    if (validateReportPath(spark,log)){
+      val inputDataForReport = getInputDataForActiveUserReport(dataPath, spark,log)
       val report = execute(getReportWithDataSetProcessing, spark, inputDataForReport)
-
-      log.debug("=======> Stage 3: Saving Report on HDFS...")
-      saveReportToHdfs(report, activeUserReportSavePath + "/DataSetReport")
+      saveActiveUserReport(report,"/dataSetReport",log)
       spark.stop
     }
     else {
-      log.error("Data for Report Creation is not available !")
+      printErrorLogs(log)
       spark.stop
       System.exit(1)
     }
--- src/main/scala/com/gd/twitteranalytics/reports/TwitterReport.scala
+++ src/main/scala/com/gd/twitteranalytics/reports/TwitterReport.scala
@@ -1,6 +1,8 @@
 package com.gd.twitteranalytics.reports
 
+import com.gd.twitteranalytics.reports.TwitterReportWriter.saveReportToHdfs
 import com.gd.twitteranalytics.util.AppConfigReader
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.log4j.Logger
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
@@ -9,8 +11,22 @@ trait TwitterReport {
   val today = java.time.LocalDate.now
   val dataPath = AppConfigReader.getAppConfigurables(0) + "/event_date=" + AppConfigReader.getReportConfigurables(1)
   val activeUserReportSavePath = AppConfigReader.getReportConfigurables(0) + "/ActiveUsers"
+  var errorMessage = ""
 
-  def getInputDataForReport(dataPath:String,spark:SparkSession):DataFrame = {
+  def validateReportPath(spark: SparkSession,log:Logger):Boolean = {
+    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+    if (!fs.exists(new Path(dataPath))){
+      errorMessage = errorMessage.concat("Data for Report Creation is not available !")
+      false
+    }
+    else {
+      log.debug("======> Stage 1: Building Report for Twitter Performance...")
+      true
+    }
+  }
+
+  def getInputDataForActiveUserReport(dataPath:String,spark:SparkSession,log:Logger):DataFrame = {
+    log.debug("=======> Stage 2: Reading Data for Twitter Active User Report")
     val tweetsDataFrame = ReportInputDataParser.getPayloadStatusAsDataFrame(spark, dataPath)
     ReportInputDataParser.getUserIdAndLocation(spark, tweetsDataFrame)
   }
@@ -20,6 +36,16 @@ trait TwitterReport {
   }
 
   def setSparkSession(name:String):SparkSession = {
-    SparkSession.builder.appName(name).getOrCreate
+    SparkSession.builder.appName(name).master("local[*]").getOrCreate
+  }
+
+  def printErrorLogs(log:Logger) = {
+    if (!errorMessage.isEmpty)
+      log.error(errorMessage)
+  }
+
+  def saveActiveUserReport(report:DataFrame,reportType:String,log:Logger)={
+    log.debug("=======> Stage 3: Saving Report on HDFS...")
+    saveReportToHdfs(report, activeUserReportSavePath + reportType)
   }
 }
