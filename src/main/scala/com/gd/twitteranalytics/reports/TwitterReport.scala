c316f4ec850739a9c9f05305001121db162d7797
shSachdeva <ssachdeva@griddynamics.com>
2018-11-06 16:35:59 -0800

    Remove MasterUrl from sparkSession

--- src/main/scala/com/gd/twitteranalytics/reports/TwitterReport.scala
+++ src/main/scala/com/gd/twitteranalytics/reports/TwitterReport.scala
@@ -33,7 +33,7 @@ trait TwitterReport {
   }
 
   def setSparkSession(name:String):SparkSession = {
-    SparkSession.builder.appName(name).master("local[*]").getOrCreate
+    SparkSession.builder.appName(name).getOrCreate
   }
 
   def printErrorLogs(log:Logger) = {
